227d59eeea6818610370c305472814ce77a0b0ca
foxostro <foxostro@gmail.com>
2019-07-27 09:32:24 -0700

    Added empty Simulator app.

---
+++ Simulator/Simulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simulator
+//
+//  Created by Andrew Fox on 7/27/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
