988d42d7b0b4da636e5e2e84b3beedb1a62763dd
foxostro <foxostro@gmail.com>
2019-07-27 12:48:35 -0700

    Continue adding additional operations to the ALU.

--- Simulator/Simulator/ALU.swift
+++ Simulator/Simulator/ALU.swift
@@ -21,9 +21,202 @@ class ALU: NSObject {
     // The actual hardware will update outputs as inputs change. However, in
     // the simulator, the outputs only update when the update method is called.
     func update() {
-        // for now, hard code the operation
-        result = a + b
-        carryFlag = (result < (Int(a) + Int(b))) ? 0 : 1
+        if (s == 0b0000) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    let r = a.addingReportingOverflow(1)
+                    result = r.0
+                    carryFlag = r.1 ? 0 : 1
+                } else {
+                    result = a
+                    carryFlag = 1
+                }
+            } else {
+                result = ~a
+                carryFlag = 1
+            }
+        } else if (s == 0b0001) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    let r = (a|b).addingReportingOverflow(1)
+                    result = r.0
+                    carryFlag = r.1 ? 0 : 1
+                } else {
+                    result = a | b
+                    carryFlag = 1
+                }
+            } else {
+                result = ~a & ~b
+                carryFlag = 1
+            }
+        } else if (s == 0b0010) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    let r = (a | ~b).addingReportingOverflow(1)
+                    result = r.0
+                    carryFlag = r.1 ? 0 : 1
+                } else {
+                    result = a | ~b
+                    carryFlag = 1
+                }
+            } else {
+                result = ~a & b
+                carryFlag = 1
+            }
+        } else if (s == 0b0011) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    result = 0
+                    carryFlag = 1
+                } else {
+                    result = ~0
+                    carryFlag = 1
+                }
+            } else {
+                result = 0
+                carryFlag = 1
+            }
+        } else if (s == 0b0100) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    let p = a.addingReportingOverflow(a & ~b)
+                    let q = p.0.addingReportingOverflow(1)
+                    result = q.0
+                    carryFlag = (p.1 || q.1) ? 0 : 1
+                } else {
+                    let r = (a & ~b).addingReportingOverflow(a | b)
+                    result = r.0
+                    carryFlag = r.1 ? 0 : 1
+                }
+            } else {
+                result = ~a & ~b
+                carryFlag = 1
+            }
+        } else if (s == 0b0101) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    let p = (a & b).addingReportingOverflow(a | b)
+                    let q = p.0.addingReportingOverflow(1)
+                    result = q.0
+                    carryFlag = (p.1 || q.1) ? 0 : 1
+                } else {
+                    let r = a.addingReportingOverflow(a & ~b)
+                    result = r.0
+                    carryFlag = r.1 ? 0 : 1
+                }
+            } else {
+                result = ~b
+                carryFlag = 1
+            }
+        } else if (s == 0b0110) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b0111) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1000) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1001) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    let p = a.addingReportingOverflow(b)
+                    let q = p.0.addingReportingOverflow(1)
+                    result = q.0
+                    carryFlag = (p.1 || q.1) ? 0 : 1
+                } else {
+                    let r = a.addingReportingOverflow(b)
+                    result = r.0
+                    carryFlag = r.1 ? 0 : 1
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1010) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1011) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1100) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1101) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1110) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else if (s == 0b1111) {
+            if (mode == 0) {
+                if (carryIn == 0) {
+                    abort()
+                } else {
+                    abort()
+                }
+            } else {
+                abort()
+            }
+        } else {
+            abort()
+        }
+        
         equalFlag = (result == 255) ? 1 : 0
     }
 }
--- Simulator/SimulatorTests/ALUTests.swift
+++ Simulator/SimulatorTests/ALUTests.swift
@@ -11,27 +11,211 @@ import XCTest
 @testable import Simulator
 
 class ALUTests: XCTestCase {
-    func testAdd() {
+    func testNotA() {
         let alu = ALU()
-        alu.a = 1
-        alu.b = 1
-        alu.s = 0b1001
-        alu.mode = 0
-        alu.carryIn = 1 // active-low so 1 -> no carry
+        alu.a = 0b10101010
+        alu.b = 0
+        alu.s = 0b0000
+        alu.mode = 1
         alu.update()
-        XCTAssertEqual(alu.result, 2)
-        XCTAssertEqual(alu.carryFlag, 1) // active-low so 1 -> no carry
+        XCTAssertEqual(alu.result, ~alu.a)
     }
     
-    func testAddWithCarry() {
+    func testIdentityA() {
         let alu = ALU()
-        alu.a = 1
-        alu.b = 1
-        alu.s = 0b1001
+        alu.a = 0b10101010
+        alu.b = 0
+        alu.s = 0b0000
         alu.mode = 0
-        alu.carryIn = 0 // active-low so 0 -> carry
+        alu.carryIn = 1
         alu.update()
-        XCTAssertEqual(alu.result, 2)
-        XCTAssertEqual(alu.carryFlag, 1) // active-low so 1 -> no carry
+        XCTAssertEqual(alu.result, alu.a)
+        XCTAssertEqual(alu.carryFlag, 1)
+    }
+    
+    func testAPlusOne() {
+        let alu = ALU()
+        alu.a = 255
+        alu.b = 0
+        alu.s = 0b0000
+        alu.mode = 0
+        alu.carryIn = 0
+        alu.update()
+        XCTAssertEqual(alu.result, 0)
+        XCTAssertEqual(alu.carryFlag, 0)
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    func testNotAOrB() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b01010101
+        alu.s = 0b0001
+        alu.mode = 1
+        alu.update()
+        XCTAssertEqual(alu.result, ~(alu.a | alu.b))
+    }
+    
+    func testAOrB() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b01010101
+        alu.s = 0b0001
+        alu.mode = 0
+        alu.carryIn = 1
+        alu.update()
+        XCTAssertEqual(alu.result, alu.a | alu.b)
+        XCTAssertEqual(alu.carryFlag, 1)
+    }
+    
+    func testAOrBPlusOne() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b01010101
+        alu.s = 0b0001
+        alu.mode = 0
+        alu.carryIn = 0
+        alu.update()
+        XCTAssertEqual(alu.result, 0b00000000)
+        XCTAssertEqual(alu.carryFlag, 0)
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    func testNotAAndB() {
+        let alu = ALU()
+        alu.a = 0b00000000
+        alu.b = 0b11111111
+        alu.s = 0b0010
+        alu.mode = 1
+        alu.update()
+        XCTAssertEqual(alu.result, 0b11111111)
+    }
+    
+    func testAOrNotB() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b00000000
+        alu.s = 0b0010
+        alu.mode = 0
+        alu.carryIn = 1
+        alu.update()
+        XCTAssertEqual(alu.result, alu.a | ~alu.b)
+    }
+    
+    func testAOrNotBPlusOne() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b10101010
+        alu.s = 0b0010
+        alu.mode = 0
+        alu.carryIn = 0
+        alu.update()
+        XCTAssertEqual(alu.result, 0)
+        XCTAssertEqual(alu.carryFlag, 0)
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    func testLogicZero() {
+        let alu = ALU()
+        alu.a = 0b11111111
+        alu.b = 0b11111111
+        alu.s = 0b0011
+        alu.mode = 1
+        alu.update()
+        XCTAssertEqual(alu.result, 0)
+    }
+    
+    func testMinusOne() {
+        let alu = ALU()
+        alu.a = 0b00000000
+        alu.b = 0b00000000
+        alu.s = 0b0011
+        alu.mode = 0
+        alu.carryIn = 1
+        alu.update()
+        XCTAssertEqual(alu.result, 0b11111111)
+    }
+    
+    func testArithmeticZero() {
+        let alu = ALU()
+        alu.a = 0b11111111
+        alu.b = 0b11111111
+        alu.s = 0b0011
+        alu.mode = 0
+        alu.carryIn = 0
+        alu.update()
+        XCTAssertEqual(alu.result, 0)
+        XCTAssertEqual(alu.carryFlag, 1)
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    func testNotAAndNotB() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b10101010
+        alu.s = 0b0100
+        alu.mode = 1
+        alu.update()
+        XCTAssertEqual(alu.result, ~alu.a & ~alu.b)
+    }
+    
+    func testAPlusAAndNotB() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b10101010
+        alu.s = 0b0101
+        alu.mode = 0
+        alu.carryIn = 1
+        alu.update()
+        XCTAssertEqual(alu.result, alu.a + (alu.a & ~alu.b))
+    }
+    
+    func testAPlusAAndNotBPlusOne() {
+        let alu = ALU()
+        alu.a = 0b11111111
+        alu.b = 0b00000000
+        alu.s = 0b0100
+        alu.mode = 0
+        alu.carryIn = 0
+        alu.update()
+        XCTAssertEqual(alu.result, 0b11111111)
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    func testNotB() {
+        let alu = ALU()
+        alu.a = 0b00000000
+        alu.b = 0b10101010
+        alu.s = 0b0101
+        alu.mode = 1
+        alu.update()
+        XCTAssertEqual(alu.result, 0b01010101)
+    }
+    
+    func testAAndNotBPlusAOrB() {
+        let alu = ALU()
+        alu.a = 0b10101010
+        alu.b = 0b10101010
+        alu.s = 0b0101
+        alu.mode = 0
+        alu.carryIn = 1
+        alu.update()
+        XCTAssertEqual(alu.result, (alu.a & ~alu.b) + (alu.a | alu.b))
+    }
+    
+    func testAAndBPlusAOrBPlusOne() {
+        let alu = ALU()
+        alu.a = 0b00000001
+        alu.b = 0b00000001
+        alu.s = 0b0101
+        alu.mode = 0
+        alu.carryIn = 0
+        alu.update()
+        XCTAssertEqual(alu.result, (alu.a & alu.b) + (alu.a | alu.b) + 1)
     }
 }
