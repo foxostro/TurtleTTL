bdc3abc9f86c278506f141cf3db0fc030b6d9cf1
foxostro <foxostro@gmail.com>
2019-07-27 10:11:24 -0700

    Added Register class

---
+++ Simulator/Simulator/Register.swift
@@ -0,0 +1,13 @@
+//
+//  Register.swift
+//  Simulator
+//
+//  Created by Andrew Fox on 7/27/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class Register: NSObject {
+    var contents: UInt8 = 0
+}
