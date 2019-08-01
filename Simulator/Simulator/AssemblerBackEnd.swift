2c7d346ca08d754afbbffe386cef24114b89cd09
foxostro <foxostro@gmail.com>
2019-07-31 18:58:47 -0700

    Add empty AssemblerBackEnd class

---
+++ Simulator/Simulator/AssemblerBackEnd.swift
@@ -0,0 +1,13 @@
+//
+//  AssemblerBackEnd.swift
+//  Simulator
+//
+//  Created by Andrew Fox on 7/31/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class AssemblerBackEnd: NSObject {
+
+}
---
+++ Simulator/SimulatorTests/AssemblerBackEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  AssemblerBackEndTests.swift
+//  SimulatorTests
+//
+//  Created by Andrew Fox on 7/31/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+
+@testable import Simulator
+
+class AssemblerBackEndTests: XCTestCase {
+    func testExample() {
+    }
+}
