846074bcb1f062dec2479f1c570425d1aeeeee14
foxostro <foxostro@gmail.com>
2019-08-01 21:38:51 -0700

    Added stub command-line tool, TurtleAssembler.

---
+++ Simulator/TurtleAssembler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TurtleAssembler
+//
+//  Created by Andrew Fox on 8/1/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
