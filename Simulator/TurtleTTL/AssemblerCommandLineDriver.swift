edb5eda474a23ba6c3a512133bfa3c3d4ef6acfb
foxostro <foxostro@gmail.com>
2019-08-02 18:11:07 -0700

    TurtleAssembler has more validation of arguments now.

--- Simulator/TurtleTTL/AssemblerCommandLineDriver.swift
+++ Simulator/TurtleTTL/AssemblerCommandLineDriver.swift
@@ -20,6 +20,8 @@ public class AssemblerCommandLineDriver: NSObject {
     
     public var status: Int32 = 1
     let arguments: [String]
+    var inputFileName: URL? = nil
+    var outputFileName: URL? = nil
     
     public required init(withArguments arguments: [String]) {
         self.arguments = arguments
@@ -40,13 +42,40 @@ public class AssemblerCommandLineDriver: NSObject {
     }
     
     func tryRun() throws {
-        try validateArguments()
+        try parseArguments()
         status = 0
     }
     
-    func validateArguments() throws {
-        if (arguments.count != 2) {
-            throw AssemblerCommandLineDriverError(format: "Expected two arguments, got %d", arguments.count)
+    func parseArguments() throws {
+        if (arguments.count != 3) {
+            throw AssemblerCommandLineDriverError(format: "Expected two arguments, got %d:\n%@", arguments.count-1, arguments.joined(separator: " "))
+        }
+        
+        try parseInputFileName()
+        try parseOutputFileName()
+    }
+
+    func parseInputFileName() throws {
+        inputFileName = URL(fileURLWithPath: arguments[1])
+        print("inputFileName: " + inputFileName!.relativePath)
+        if !FileManager.default.fileExists(atPath: inputFileName!.relativePath) {
+            throw AssemblerCommandLineDriverError(format: "Input file does not exist: %@", inputFileName!.relativePath)
+        }
+        if !FileManager.default.isReadableFile(atPath: inputFileName!.relativePath) {
+            throw AssemblerCommandLineDriverError(format: "Input file is not readable: %@", inputFileName!.relativePath)
+        }
+    }
+    
+    func parseOutputFileName() throws {
+        outputFileName = URL(fileURLWithPath: arguments[2])
+        print("outputFileName: " + outputFileName!.relativePath)
+        if !FileManager.default.fileExists(atPath: outputFileName!.deletingLastPathComponent().relativePath) {
+            throw AssemblerCommandLineDriverError(format: "Specified output directory does not exist: %@", outputFileName!.deletingLastPathComponent().relativePath)
+        }
+        if FileManager.default.fileExists(atPath: outputFileName!.relativePath) {
+            if !FileManager.default.isWritableFile(atPath: outputFileName!.relativePath) {
+                throw AssemblerCommandLineDriverError(format: "Output file exists but is not writable: %@", inputFileName!.relativePath)
+            }
         }
     }
 }
