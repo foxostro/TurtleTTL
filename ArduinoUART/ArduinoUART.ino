66b8531a436dd6497af70e83245f4535afbc02cb
foxostro <foxostro@gmail.com>
2019-08-17 20:05:23 -0700

    Arduino should watch the clock pulse too.

--- ArduinoUART/ArduinoUART.ino
+++ ArduinoUART/ArduinoUART.ino
@@ -10,14 +10,10 @@ const byte pinBus6 = 10;
 const byte pinBus7 = 11;
 const byte pinSEL = 12;
 const byte pinAddr0 = 13;
-
-volatile bool isPendingOutput = false;
-volatile bool isPendingInput = false;
+const byte pinCLK = A0;
 
 void setup() {
-  Serial.begin(57600);
-  while (!Serial);
-
+  pinMode(pinSEL, INPUT);
   pinMode(pinMO, INPUT);
   pinMode(pinMI, INPUT);
   pinMode(pinBus0, INPUT);
@@ -28,82 +24,40 @@ void setup() {
   pinMode(pinBus5, INPUT);
   pinMode(pinBus6, INPUT);
   pinMode(pinBus7, INPUT);
-  pinMode(pinSEL, INPUT);
   pinMode(pinAddr0, INPUT);
+  pinMode(pinCLK, INPUT);
+  
+  Serial.begin(57600);
+  while (!Serial);
   
   Serial.println("Beginning...");
-  
-  attachInterrupt(digitalPinToInterrupt(pinMO), markPendingOutput, FALLING);
-  attachInterrupt(digitalPinToInterrupt(pinMI), markPendingInput, FALLING);
 }
 
 void loop() {
-  Serial.println("Waiting for signal");
-  while (!isPendingOutput && !isPendingInput);
-
-  noInterrupts();
-
-  Serial.println("Saw a signal");
-
-  if (isPendingOutput) {
-    Serial.println("Output?");
-    const bool sel = (LOW == digitalRead(pinSEL));
-    const bool mo = (LOW == digitalRead(pinMO));
-    
-    if (sel && mo) {
-      pinMode(pinBus0, OUTPUT);
-      pinMode(pinBus1, OUTPUT);
-      pinMode(pinBus2, OUTPUT);
-      pinMode(pinBus3, OUTPUT);
-      pinMode(pinBus4, OUTPUT);
-      pinMode(pinBus5, OUTPUT);
-      pinMode(pinBus6, OUTPUT);
-      pinMode(pinBus7, OUTPUT);
-
-      const byte addr0 = digitalRead(pinAddr0);
-
-      int value = 0;
-      if (LOW == addr0) {
-        value = Serial.read();
-      } else {
-        value = Serial.available();
-      }
-
-      digitalWrite(pinBus0, (value & 1) != 0);
-      digitalWrite(pinBus1, (value & 2) != 0);
-      digitalWrite(pinBus2, (value & 4) != 0);
-      digitalWrite(pinBus3, (value & 8) != 0);
-      digitalWrite(pinBus4, (value & 16) != 0);
-      digitalWrite(pinBus5, (value & 32) != 0);
-      digitalWrite(pinBus6, (value & 64) != 0);
-      digitalWrite(pinBus7, (value & 128) != 0);
-
-      // Wait for the next control signal falling edge.
-      // The above may take more than one clock cycle. The computer is
-      // expected to hold the signals for the amount of time needed to
-      // ensure there will be a signal transition here.
-      while ((LOW == digitalRead(pinMO)) && (LOW == digitalRead(pinSEL)));
-    }
-    
-    pinMode(pinBus0, INPUT);
-    pinMode(pinBus1, INPUT);
-    pinMode(pinBus2, INPUT);
-    pinMode(pinBus3, INPUT);
-    pinMode(pinBus4, INPUT);
-    pinMode(pinBus5, INPUT);
-    pinMode(pinBus6, INPUT);
-    pinMode(pinBus7, INPUT);
-    
-    isPendingOutput = false;
+  bool clk, sel, mi, mo, addr0;
+  
+  while (true == (sel = digitalRead(pinSEL))) {
+    Serial.print("SEL: ");
+    Serial.print(sel);
+    Serial.print("\n");
   }
 
-  if (isPendingInput) {
-    Serial.println("Input?");
-    const bool sel = (LOW == digitalRead(pinSEL));
-    const bool mi = (LOW == digitalRead(pinMI));
-    const byte addr0 = digitalRead(pinAddr0);
+  while (true) {
+    clk = digitalRead(pinCLK);
+    if (clk) {
+      break;
+    }
+    
+    mo = digitalRead(pinMO);
+    if (!mo) {
+      break;
+    }
+  }
 
-    if (sel && mi && (HIGH == addr0)) {
+  if (clk) {
+    sel = digitalRead(pinSEL);
+    mi = digitalRead(pinMI);
+    if (!sel && !mi) {
       const byte bus = (digitalRead(pinBus0))
                      | (digitalRead(pinBus1) << 1)
                      | (digitalRead(pinBus2) << 2)
@@ -117,18 +71,40 @@ void loop() {
       Serial.print(bus);
       Serial.print("\n");
     }
-    
-    isPendingInput = false;
+  } else {
+    Serial.println("I think we just saw the MO signal go active.");
+    sel = digitalRead(pinSEL);
+    mo = digitalRead(pinMO);
+    if (!sel && !mo) {
+      pinMode(pinBus0, OUTPUT);
+      pinMode(pinBus1, OUTPUT);
+      pinMode(pinBus2, OUTPUT);
+      pinMode(pinBus3, OUTPUT);
+      pinMode(pinBus4, OUTPUT);
+      pinMode(pinBus5, OUTPUT);
+      pinMode(pinBus6, OUTPUT);
+      pinMode(pinBus7, OUTPUT);
+
+      addr0 = digitalRead(pinAddr0);
+
+      int value = 0;
+      if (addr0) {
+        value = Serial.available();
+      } else {
+        value = Serial.read();
+      }
+
+      digitalWrite(pinBus0, (value & 1) != 0);
+      digitalWrite(pinBus1, (value & 2) != 0);
+      digitalWrite(pinBus2, (value & 4) != 0);
+      digitalWrite(pinBus3, (value & 8) != 0);
+      digitalWrite(pinBus4, (value & 16) != 0);
+      digitalWrite(pinBus5, (value & 32) != 0);
+      digitalWrite(pinBus6, (value & 64) != 0);
+      digitalWrite(pinBus7, (value & 128) != 0);
+    }
   }
 
-  interrupts();
-
-}
-
-void markPendingOutput() {
-  isPendingOutput = true;
-}
-
-void markPendingInput() {
-  isPendingInput = true;
+  // Wait for the next falling edge of the clock.
+  while (digitalRead(pinCLK));
 }
