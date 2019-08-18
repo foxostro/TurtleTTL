f7718e7b83890f625fbcad8aac3624410e1ae425
foxostro <foxostro@gmail.com>
2019-08-17 20:24:19 -0700

    When outputting, return the bus I/O pins to INPUT when done.

--- ArduinoSerialInterface/ArduinoSerialInterface.ino
+++ ArduinoSerialInterface/ArduinoSerialInterface.ino
@@ -29,18 +29,21 @@ void setup() {
   
   Serial.begin(57600);
   while (!Serial);
-  
-  Serial.println("Beginning...");
 }
 
 void loop() {
   bool clk, sel, mi, mo, addr0;
-  
-  while (true == (sel = digitalRead(pinSEL)));
 
+  // Wait for either MO or MI to become active.
   while (true) {
-    clk = digitalRead(pinCLK);
-    if (clk) {
+    // Make sure the device is selected too.
+    sel = digitalRead(pinSEL);
+    if (sel) {
+      return;
+    }
+    
+    mi = digitalRead(pinMI);
+    if (!mi) {
       break;
     }
     
@@ -50,57 +53,62 @@ void loop() {
     }
   }
 
-  if (clk) {
-    sel = digitalRead(pinSEL);
-    mi = digitalRead(pinMI);
-    if (!sel && !mi) {
-      const byte bus = (digitalRead(pinBus0))
-                     | (digitalRead(pinBus1) << 1)
-                     | (digitalRead(pinBus2) << 2)
-                     | (digitalRead(pinBus3) << 3)
-                     | (digitalRead(pinBus4) << 4)
-                     | (digitalRead(pinBus5) << 5)
-                     | (digitalRead(pinBus6) << 6)
-                     | (digitalRead(pinBus7) << 7);
-                     
-      Serial.print("got: ");
-      Serial.print(bus);
-      Serial.print("\n");
-    }
+  if (!mi) {
+    // Wait for the rising edge of the clock before reading the bus.
+    while (!digitalRead(pinCLK));
+    
+    const byte bus = (digitalRead(pinBus0))
+                   | (digitalRead(pinBus1) << 1)
+                   | (digitalRead(pinBus2) << 2)
+                   | (digitalRead(pinBus3) << 3)
+                   | (digitalRead(pinBus4) << 4)
+                   | (digitalRead(pinBus5) << 5)
+                   | (digitalRead(pinBus6) << 6)
+                   | (digitalRead(pinBus7) << 7);
+                   
+    Serial.print("got: ");
+    Serial.print(bus);
+    Serial.print("\n");
   } else {
     Serial.println("I think we just saw the MO signal go active.");
-    sel = digitalRead(pinSEL);
-    mo = digitalRead(pinMO);
-    if (!sel && !mo) {
-      pinMode(pinBus0, OUTPUT);
-      pinMode(pinBus1, OUTPUT);
-      pinMode(pinBus2, OUTPUT);
-      pinMode(pinBus3, OUTPUT);
-      pinMode(pinBus4, OUTPUT);
-      pinMode(pinBus5, OUTPUT);
-      pinMode(pinBus6, OUTPUT);
-      pinMode(pinBus7, OUTPUT);
+    pinMode(pinBus0, OUTPUT);
+    pinMode(pinBus1, OUTPUT);
+    pinMode(pinBus2, OUTPUT);
+    pinMode(pinBus3, OUTPUT);
+    pinMode(pinBus4, OUTPUT);
+    pinMode(pinBus5, OUTPUT);
+    pinMode(pinBus6, OUTPUT);
+    pinMode(pinBus7, OUTPUT);
 
-      addr0 = digitalRead(pinAddr0);
+    addr0 = digitalRead(pinAddr0);
 
-      int value = 0;
-      if (addr0) {
-        value = Serial.available();
-      } else {
-        value = Serial.read();
-      }
-
-      digitalWrite(pinBus0, (value & 1) != 0);
-      digitalWrite(pinBus1, (value & 2) != 0);
-      digitalWrite(pinBus2, (value & 4) != 0);
-      digitalWrite(pinBus3, (value & 8) != 0);
-      digitalWrite(pinBus4, (value & 16) != 0);
-      digitalWrite(pinBus5, (value & 32) != 0);
-      digitalWrite(pinBus6, (value & 64) != 0);
-      digitalWrite(pinBus7, (value & 128) != 0);
+    int value = 0;
+    if (addr0) {
+      value = Serial.available();
+    } else {
+      value = Serial.read();
     }
+
+    digitalWrite(pinBus0, (value & 1) != 0);
+    digitalWrite(pinBus1, (value & 2) != 0);
+    digitalWrite(pinBus2, (value & 4) != 0);
+    digitalWrite(pinBus3, (value & 8) != 0);
+    digitalWrite(pinBus4, (value & 16) != 0);
+    digitalWrite(pinBus5, (value & 32) != 0);
+    digitalWrite(pinBus6, (value & 64) != 0);
+    digitalWrite(pinBus7, (value & 128) != 0);
   }
 
   // Wait for the next falling edge of the clock.
   while (digitalRead(pinCLK));
+
+  // Stop driving the bus.
+  pinMode(pinBus0, INPUT);
+  pinMode(pinBus1, INPUT);
+  pinMode(pinBus2, INPUT);
+  pinMode(pinBus3, INPUT);
+  pinMode(pinBus4, INPUT);
+  pinMode(pinBus5, INPUT);
+  pinMode(pinBus6, INPUT);
+  pinMode(pinBus7, INPUT);
 }
