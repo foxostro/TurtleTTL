6054036ce1853d3b4495497fc6f30638fb65389e
foxostro <foxostro@gmail.com>
2019-11-07 19:32:24 -0800

    optimize Arduino sketch to improve responsiveness

--- ArduinoSerialInterface/ArduinoSerialInterface.ino
+++ ArduinoSerialInterface/ArduinoSerialInterface.ino
@@ -1,29 +1,7 @@
-const byte pinPI = 2;
-const byte pinPO = 3;
-const byte pinBus0 = 4;
-const byte pinBus1 = 5;
-const byte pinBus2 = 6;
-const byte pinBus3 = 7;
-const byte pinBus4 = 8;
-const byte pinBus5 = 9;
-const byte pinBus6 = 10;
-const byte pinBus7 = 11;
-const byte pinAddr0 = 13;
-const byte pinCLK = A0;
-
 void setup() {
-  pinMode(pinPI, INPUT);
-  pinMode(pinPO, INPUT);
-  pinMode(pinBus0, INPUT);
-  pinMode(pinBus1, INPUT);
-  pinMode(pinBus2, INPUT);
-  pinMode(pinBus3, INPUT);
-  pinMode(pinBus4, INPUT);
-  pinMode(pinBus5, INPUT);
-  pinMode(pinBus6, INPUT);
-  pinMode(pinBus7, INPUT);
-  pinMode(pinAddr0, INPUT);
-  pinMode(pinCLK, INPUT);
+  DDRD &= 0b00000011; // Set pins 2 through 7 to input.
+  DDRB &= 0b11000000; // Set pins 8-13 for input.
+  DDRC &= 0b11111110; // Set analog pin 0 for input.
   
   Serial.begin(57600);
   while (!Serial);
@@ -31,16 +9,17 @@ void setup() {
 }
 
 void loop() {
-  bool clk, sel, pi, po, addr0;
+  bool clk, sel, pi, po;
 
   // Wait for either PO or PI to become active.
   while (true) {
-    pi = digitalRead(pinPI);
+    const byte d = PIND;
+    pi = (d & 0b00000100) != 0; // pin 2 is PI
     if (!pi) {
       break;
     }
     
-    po = digitalRead(pinPO);
+    po = (d & 0b00001000) != 0; // pin 3 is PO
     if (!po) {
       break;
     }
@@ -48,61 +27,39 @@ void loop() {
 
   if (!pi) {
     // Wait for the rising edge of the clock before reading the bus.
-    while (!digitalRead(pinCLK));
-    
-    const byte bus = (digitalRead(pinBus0))
-                   | (digitalRead(pinBus1) << 1)
-                   | (digitalRead(pinBus2) << 2)
-                   | (digitalRead(pinBus3) << 3)
-                   | (digitalRead(pinBus4) << 4)
-                   | (digitalRead(pinBus5) << 5)
-                   | (digitalRead(pinBus6) << 6)
-                   | (digitalRead(pinBus7) << 7);
+    while((PINC & 1) == 0);
+
+    // Read the value of the bus.
+    // The eight bits of the bus map to pins 4 through 11.
+    const byte bus = ((PIND >> 4) & 0b00001111) | ((PINB & 0b00001111) << 4);
 
     Serial.print((char)bus);
 
     // Wait for the clock falling edge. The bus value becomes invalid after this point.
-    while (digitalRead(pinCLK));
+    while((PINC & 1) != 0);
   } else if (!po) {
-    pinMode(pinBus0, OUTPUT);
-    pinMode(pinBus1, OUTPUT);
-    pinMode(pinBus2, OUTPUT);
-    pinMode(pinBus3, OUTPUT);
-    pinMode(pinBus4, OUTPUT);
-    pinMode(pinBus5, OUTPUT);
-    pinMode(pinBus6, OUTPUT);
-    pinMode(pinBus7, OUTPUT);
+    DDRD |= 0b11110000; // Set pins 2 through 7 to output.
+    DDRB |= 0b00001111; // Set pins 8-13 for output.
 
-    addr0 = digitalRead(pinAddr0);
-
+    // Read pin 13 (ADDR0). If it's non-zero then get the number of available bytes.
     int value = 0;
-    if (addr0) {
+    if (PINB & 0b00100000) {
       value = Serial.available();
     } else {
       value = Serial.read();
     }
 
-    digitalWrite(pinBus0, (value>>0) & 1);
-    digitalWrite(pinBus1, (value>>1) & 1);
-    digitalWrite(pinBus2, (value>>2) & 1);
-    digitalWrite(pinBus3, (value>>3) & 1);
-    digitalWrite(pinBus4, (value>>4) & 1);
-    digitalWrite(pinBus5, (value>>5) & 1);
-    digitalWrite(pinBus6, (value>>6) & 1);
-    digitalWrite(pinBus7, (value>>7) & 1);
+    // Output a value to the bus.
+    // The eight bits of the bus map to pins 4 through 11.
+    PORTD = (value & 0b00001111) << 4;
+    PORTB = (value & 0b11110000) >> 4;
 
     // Hold the value on the bus for the duration of the register clock pulse.
-    while (!digitalRead(pinCLK));
-    while (digitalRead(pinCLK));
+    while((PINC & 1) == 0);
+    while((PINC & 1) != 0);
 
     // Stop driving the bus.
-    pinMode(pinBus0, INPUT);
-    pinMode(pinBus1, INPUT);
-    pinMode(pinBus2, INPUT);
-    pinMode(pinBus3, INPUT);
-    pinMode(pinBus4, INPUT);
-    pinMode(pinBus5, INPUT);
-    pinMode(pinBus6, INPUT);
-    pinMode(pinBus7, INPUT);
+    DDRD &= 0b00000011; // Set pins 2 through 7 to input.
+    DDRB &= 0b11000000; // Set pins 8-13 for input.
   }
 }
