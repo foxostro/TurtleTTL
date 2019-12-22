2887868f0487b34e5e47e47d1aa60dfb148f0931
foxostro <foxostro@gmail.com>
2019-12-22 14:31:34 -0800

    No need to keep the old serial interface sketch around.

--- ArduinoSerialInterface/ArduinoSerialInterface.ino
+++ ArduinoSerialInterface/ArduinoSerialInterface.ino
@@ -1,107 +1,139 @@
-const byte pinPO = 2;
-const byte pinPI = 3;
-const byte pinBus1 = 4;
-const byte pinBus0 = 5;
-const byte pinBus3 = 6;
-const byte pinBus2 = 7;
-const byte pinBus5 = 8;
-const byte pinBus4 = 9;
-const byte pinBus7 = 10;
-const byte pinBus6 = 11;
-const byte pinAddr1 = 13;
+const int kPinSCK = 2;
+const int kPinMOSI[8] = {3, 4, 5, 6, 7, 8, 9, 10};
+const int kPinMISO[8]  = {11, 12, 13, A0, A1, A2, A3, A4};
+const byte kStatusSuccess = 0xff;
+enum State {
+  kStateIdle = 0,
+  kStateWaitingForOutputByte
+};
+enum Command {
+  kCommandResetSerialLink = 0,
+  kCommandPutByte,
+  kCommandGetByte,
+  kCommandGetNumBytes
+};
+
+State g_state = kStateIdle;
+
+void set_miso(byte value) {
+  for (int i = 0; i < 8; ++i) {
+    digitalWrite(kPinMISO[i], (value >> i) & 1);
+  }
+}
+
+byte get_mosi() {
+  byte value = 0;
+  for (int i = 0; i < 8; ++i) {
+    value |= (digitalRead(kPinMOSI[i]) << i);
+  }
+  return value;
+}
+
+void wait_for_sck_rising_edge() {
+  bool sck;
+  while ((sck = digitalRead(kPinSCK)) == 0);
+}
+
+void wait_for_sck_falling_edge() {
+  bool sck;
+  while ((sck = digitalRead(kPinSCK)) != 0);
+}
 
 void setup() {
-  pinMode(pinPI, INPUT);
-  pinMode(pinPO, INPUT);
-  pinMode(pinBus0, INPUT);
-  pinMode(pinBus1, INPUT);
-  pinMode(pinBus2, INPUT);
-  pinMode(pinBus3, INPUT);
-  pinMode(pinBus4, INPUT);
-  pinMode(pinBus5, INPUT);
-  pinMode(pinBus6, INPUT);
-  pinMode(pinBus7, INPUT);
-  pinMode(pinAddr1, INPUT);
+  pinMode(kPinSCK, INPUT_PULLUP);
+  for (int i = 0; i < 8; ++i) {
+    pinMode(kPinMOSI[i], INPUT_PULLUP);
+  }
+  set_miso(0);
+  for (int i = 0; i < 8; ++i) {
+    pinMode(kPinMISO[i], OUTPUT);
+  }
   
   Serial.begin(57600);
   while (!Serial);
   Serial.println("Arduino ready.");
 }
 
+State doCommandReset() {
+  set_miso(kStatusSuccess);
+}
+
+State doCommandPutByte() {
+  set_miso(kStatusSuccess);
+  return kStateWaitingForOutputByte;
+}
+
+State doCommandGetByte() {
+  byte value = Serial.read();
+  set_miso(value);
+  return kStateIdle;
+}
+
+State doCommandGetNumBytes() {
+  byte value = Serial.available();
+  set_miso(value);
+  return kStateIdle;
+}
+
+State doStateIdle(byte mosi) {
+  State nextState = kStateIdle;
+  switch (mosi) {
+    case kCommandResetSerialLink:
+      //Serial.println("kCommandResetSerialLink");
+      nextState = doCommandReset();
+      break;
+      
+    case kCommandPutByte:
+      //Serial.println("kCommandPutByte");
+      nextState = doCommandPutByte();
+      break;
+      
+    case kCommandGetByte:
+      //Serial.println("kCommandGetByte");
+      nextState = doCommandGetByte();
+      break;
+      
+    case kCommandGetNumBytes:
+      //Serial.println("kCommandGetNumBytes");
+      nextState = doCommandGetNumBytes();
+      break;
+  }
+  return nextState;
+}
+
+State doStateWaiting(byte mosi) {
+  //Serial.print("put byte: ");
+  Serial.print((char)mosi);
+  //Serial.print("\n");
+  set_miso(kStatusSuccess);
+  return kStateIdle;
+}
+
 void loop() {
-  bool pi, po, addr1;
-
-  // Wait for either PO or PI to become active.
-  while (true) {
-    pi = digitalRead(pinPI);
-    if (!pi) {
-      break;
-    }
+  wait_for_sck_rising_edge();
+  //Serial.println("rising edge of SCK");
+  
+  byte mosi = get_mosi();
+  State nextState;
+  
+  //Serial.print("mosi: ");
+  //Serial.print((int)mosi);
+  //Serial.print("\n");
+  
+  switch (g_state) {
+    case kStateIdle:
+      //Serial.println("kStateIdle");
+      nextState = doStateIdle(mosi);
+    break;
     
-    po = digitalRead(pinPO);
-    if (!po) {
-      break;
-    }
+    case kStateWaitingForOutputByte:
+      //Serial.println("kStateWaitingForOutputByte");
+      nextState = doStateWaiting(mosi);
+    break;
   }
-
-  addr1 = digitalRead(pinAddr1);
-
-  if (!pi) {
-    const byte bus = (digitalRead(pinBus0))
-                   | (digitalRead(pinBus1) << 1)
-                   | (digitalRead(pinBus2) << 2)
-                   | (digitalRead(pinBus3) << 3)
-                   | (digitalRead(pinBus4) << 4)
-                   | (digitalRead(pinBus5) << 5)
-                   | (digitalRead(pinBus6) << 6)
-                   | (digitalRead(pinBus7) << 7);
-
-                   
-    int value = 0;
-    if (addr1) {
-      Serial.begin(57600);
-      while (!Serial);
-      Serial.println("Arduino ready.");
-    } else {
-      Serial.print((char)bus);
-    }
-
-    while (!digitalRead(pinPI));
-  } else if (!po) {
-    int value = 0;
-    if (addr1) {
-      value = Serial.available();
-    } else {
-      value = Serial.read();
-    }
-
-    digitalWrite(pinBus0, (value>>0) & 1);
-    digitalWrite(pinBus1, (value>>1) & 1);
-    digitalWrite(pinBus2, (value>>2) & 1);
-    digitalWrite(pinBus3, (value>>3) & 1);
-    digitalWrite(pinBus4, (value>>4) & 1);
-    digitalWrite(pinBus5, (value>>5) & 1);
-    digitalWrite(pinBus6, (value>>6) & 1);
-    digitalWrite(pinBus7, (value>>7) & 1);
-    
-    pinMode(pinBus0, OUTPUT);
-    pinMode(pinBus1, OUTPUT);
-    pinMode(pinBus2, OUTPUT);
-    pinMode(pinBus3, OUTPUT);
-    pinMode(pinBus4, OUTPUT);
-    pinMode(pinBus5, OUTPUT);
-    pinMode(pinBus6, OUTPUT);
-    pinMode(pinBus7, OUTPUT);
-
-    while (!digitalRead(pinPO));
-
-    pinMode(pinBus0, INPUT);
-    pinMode(pinBus1, INPUT);
-    pinMode(pinBus2, INPUT);
-    pinMode(pinBus3, INPUT);
-    pinMode(pinBus4, INPUT);
-    pinMode(pinBus5, INPUT);
-    pinMode(pinBus6, INPUT);
-    pinMode(pinBus7, INPUT);
-  }
+  
+  g_state = nextState;
+  
+  wait_for_sck_falling_edge();
+  //Serial.println("falling edge of SCK");
 }
